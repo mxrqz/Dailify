220b453e45bb27a8f7e96b804aa54f01ca5eab7d
mxrqz <msw940220@gmail.com>
2026-08-31 21:55:38 -0300

    feat(quotas): registro unico declara as tres quotas e seus escopos
    Dailify-5v4

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export * from "./types";
 export * from "./hash";
 export * from "./limits";
 export * from "./pricing";
+export * from "./quotas";
 export * from "./recurrence";
 export * from "./dates";
